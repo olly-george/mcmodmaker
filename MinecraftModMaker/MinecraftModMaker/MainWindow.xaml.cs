18a8eb8a4425fcb08280cff4a2513f5ef9c28464
olly <olly.george@gmail.com>
2019-04-25 16:26:58 +0100

    Did some stuff

--- MinecraftModMaker/MinecraftModMaker/MainWindow.xaml.cs
+++ MinecraftModMaker/MinecraftModMaker/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MinecraftModMaker
+﻿using System.Windows;
+
+namespace MinecraftModMaker
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +10,11 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItemNewBlock_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New block was selected. Do you wish to procceed?");
         }
     }
 }
